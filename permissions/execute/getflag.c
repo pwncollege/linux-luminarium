b75bc4769fda1672083968f1e93a8cd9b99c179e
Pric3 <minhnhatbg05@gmail.com>
2024-02-08 11:56:06 +0700

    Hide the flag better this time around

--- permissions/execute/getflag.c
+++ permissions/execute/getflag.c
@@ -2,7 +2,7 @@
 
 int main() {
   FILE *file;
-  file = fopen("/flag", "r");
+  file = fopen("/etc/ctfconf", "r");
   char ch;
   printf("\nHere's your flag: ");
   while ((ch = fgetc(file)) != EOF) {
